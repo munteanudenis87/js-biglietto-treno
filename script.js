113fe733686943e02f407f10acd9dda77a7e920e
munteanudenis87 <munteanudenis87@gmail.com>
2025-12-03 21:50:08 +0100

    aggiunto toFixed(2)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const distanza = parseInt(prompt("inserisci la distanza da percorrere"));
 const età = parseInt(prompt("età del passeggero"));
 const prezzoBiglietto = 0.21 * distanza;
-console.log(prezzoBiglietto);
+//console.log(prezzoBiglietto);
 let scontoMin = 20 / 100 * prezzoBiglietto;
 let scontoOv = 40 / 100 * prezzoBiglietto;
 let scontoMinorenni;
@@ -9,10 +9,10 @@ let scontoOver65;
 
 if(età < 18){
     let scontoMinorenni = prezzoBiglietto - scontoMin;
-    console.log(`prezzo del biglietto ${scontoMinorenni}`);
+    console.log(`prezzo del biglietto ${scontoMinorenni.toFixed(2)}`);
 } else if(età > 65){
     let scontoOver65 = prezzoBiglietto - scontoOv;
-    console.log(`prezzo del biglietto ${scontoOver65}`);
+    console.log(`prezzo del biglietto ${scontoOver65.toFixed(2)}`);
 } else {
-    console.log(`prezzo del biglietto ${prezzoBiglietto}`);
+    console.log(`prezzo del biglietto ${prezzoBiglietto.toFixed(2)}`);
 }
