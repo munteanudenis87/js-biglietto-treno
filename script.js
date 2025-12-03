d661b642bce4e8034b9af3d11e788314c62f0485
munteanudenis87 <munteanudenis87@gmail.com>
2025-12-03 21:33:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const distanza = parseInt(prompt("inserisci la distanza da percorrere"));
+const età = parseInt(prompt("età del passeggero"));
+const prezzoBiglietto = 0.21 * distanza;
+console.log(prezzoBiglietto);
+let scontoMin = 20 / 100 * prezzoBiglietto;
+let scontoOv = 40 / 100 * prezzoBiglietto;
+let scontoMinorenni;
+let scontoOver65;
+
+if(età < 18){
+    let scontoMinorenni = prezzoBiglietto - scontoMin;
+    console.log(`prezzo del biglietto ${scontoMinorenni}`);
+} else if(età > 65){
+    let scontoOver65 = prezzoBiglietto - scontoOv;
+    console.log(`prezzo del biglietto ${scontoOver65}`);
+} else {
+    console.log(`prezzo del biglietto ${prezzoBiglietto}`);
+}
